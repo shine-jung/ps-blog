164aed6285d9e6a077df3f73edb6d326a4e91d5e
shine-jung <79754613+shine-jung@users.noreply.github.com>
2022-09-27 00:56:04 +0900

    feat: implement search function

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -5,9 +5,12 @@ import { IPostContent } from "../types/types";
 import PostList from "../components/PostList";
 import { Container, Typography } from "@mui/material";
 import { Loader } from "../components/components";
+import { useRecoilValue } from "recoil";
+import { searchTermState } from "../services/atoms";
 
 function Main() {
   const [init, setInit] = useState(false);
+  const searchTerm = useRecoilValue(searchTermState);
   const [postContents, setPostContents] = useState<IPostContent[]>();
   const getPostContents = async () => {
     const postsCol = collection(db, "posts");
@@ -31,9 +34,17 @@ function Main() {
       {init ? (
         <Container component="main" maxWidth="lg">
           <Typography variant="h4" sx={{ mb: 4 }}>
-            전체 글
+            {searchTerm === "" ? "전체 글" : `'${searchTerm}'의 검색 결과`}
           </Typography>
-          <PostList postContents={postContents} maxColum={3} />
+          <PostList
+            postContents={postContents?.filter(
+              (item) =>
+                item.title?.includes(searchTerm) ||
+                item.tags?.toString().includes(searchTerm) ||
+                item.description?.includes(searchTerm)
+            )}
+            maxColum={3}
+          />
         </Container>
       ) : (
         <Loader>Loading...</Loader>
--- src/services/atoms.ts
+++ src/services/atoms.ts
@@ -4,3 +4,8 @@ export const isDarkAtom = atom({
   key: "isDark",
   default: localStorage.getItem("darkMode") === "true",
 });
+
+export const searchTermState = atom({
+  key: "searchTerm",
+  default: "",
+});
